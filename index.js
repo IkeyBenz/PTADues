2860d258977c9d59b2277493c711fdec3c446e1c
Ikey Benzaken <ikey.benz@gmail.com>
2018-10-07 15:28:48 -0700

    Got rid of key

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let bodyParser = require('body-parser');
 let firebase = require('firebase');
 let methodOverride = require('method-override');
 
-let app = express();
-let port = process.env.PORT || 5000;
-firebase.initializeApp(require('./firebaseConfig'));
+const activateControllers = require('./controllers/main');
+const app = express();
+const port = process.env.PORT || 5000;
 
-let activateControllers = require('./controllers/main');
+firebase.initializeApp(process.env.FIREBASE_CONFIG || require('./firebaseConfig'));
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars')
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,16 +1,15 @@
 var StripeHandler = StripeCheckout.configure({
-    key: 'pk_live_Vek1WDHXK9AjMQcZJxaQSVEY',
+    key: 'getPublicKey',
     image: '/images/Logo.png',
     locale: 'auto',
     token: function(tkn) {
         let amnt = $('#paymentAmount').val();
         $.ajax({
-        url: `https://jsc-payment-processor.herokuapp.com/charge`, 
+        url: `/charge/`, 
         data: { token: tkn.id, amount: amnt },
         statusCode: {
             200: function() {
-            alert('Payment Succeeded\nPlease sign the following waiver to complete your signup.');
-            window.location.replace('https://waiver.fr/p-FuRDa');
+            alert('Payment Succeeded\nThank you for your generous contribution!');
             },
             500: function() {
             alert('Something went wrong.');
