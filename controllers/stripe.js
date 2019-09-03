3d3ddd6c97b9efc19bac223f97e7f8ec066a8480
Ikey Benzaken <ikey.benz@gmail.com>
2019-09-03 15:06:42 -0700

    Updated description for pta dues stripe charge

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -2,19 +2,19 @@ const stripe = require('stripe')(process.env.STRIPE_PRIVATE);
 
 module.exports = function (app) {
 
-    app.post('/charge/', (req, res) => {
-        if (!req.body.token) { return res.status(400).send('No token, cannot process').end() }
-        stripe.charges.create({
-            amount: req.body.amount,
-            currency: 'usd',
-            description: 'PTA Purim Gifts',
-            source: req.body.token
-        }).then(chrg => {
-            res.status(200).end();
-        }).catch(err => {
-            console.error(err);
-            res.status(500).end(err);
-        });
+  app.post('/charge/', (req, res) => {
+    if (!req.body.token) { return res.status(400).send('No token, cannot process').end() }
+    stripe.charges.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      description: 'PTA DUES',
+      source: req.body.token
+    }).then(chrg => {
+      res.status(200).end();
+    }).catch(err => {
+      console.error(err);
+      res.status(500).end(err);
     });
+  });
 
 }
